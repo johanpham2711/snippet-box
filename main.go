3617404b3f16ee4e81d02b224cacb93eabe683a3
Johan Pham <johanpham2711@gmail.com>
2025-03-07 15:58:27 +0700

    feat: basics web app

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippet Box"))
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("The application is healthy!"))
+}
 
 func main() {
-	fmt.Println("Hello from Snippet Box!")
+	loadEnv()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/healthz", healthCheck)
+
+	serverPort := os.Getenv("SERVER_PORT")
+	if serverPort == "" {
+		serverPort = "8080"
+	}
+
+	log.Println("The application is starting on port", serverPort)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", serverPort), mux)
+	log.Fatal(err)
 }
