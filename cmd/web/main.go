666b2108e82874e48ed2330186f7d1bafc911a42
Johan Pham <johanpham2711@gmail.com>
2025-03-09 23:59:26 +0700

    feat: create db connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -27,13 +30,37 @@ func main() {
 	fileServer := http.FileServer(http.Dir("ui/static/"))
 	http.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
+	// Connect to the database
+	dsn := os.Getenv("MYSQL_DSN")
+	db, err := openDB(dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	// Start the server
 	serverPort := os.Getenv("SERVER_PORT")
 	if serverPort == "" {
 		serverPort = "8080"
 	}
 	logger.Info("starting server", "addr", serverPort)
-	err := http.ListenAndServe(fmt.Sprintf(":%s", serverPort), app.routes())
-	logger.Error(err.Error())
+	serverErr := http.ListenAndServe(fmt.Sprintf(":%s", serverPort), app.routes())
+	logger.Error(serverErr.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
