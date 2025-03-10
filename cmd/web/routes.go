54efcc052b5420f5344a2d8a6d92ec9daeee7abf
Johan Pham <johanpham2711@gmail.com>
2025-03-10 13:58:40 +0700

    feat: executing sql statements

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,6 +9,7 @@ func (app *application) routes() *http.ServeMux {
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /healthz", app.healthCheck)
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
