e62c66b47642d73d037725db42c93db2a0459ce8
Johan Pham <johanpham2711@gmail.com>
2025-03-11 13:48:14 +0700

    feat: e2e test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io"
+	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,7 +11,7 @@ import (
 	"github.com/johanpham2711/snippet-box/internal/assert"
 )
 
-func TestPing(t *testing.T) {
+func TestPingUT(t *testing.T) {
 	// Initialize a new httptest.ResponseRecorder.
 	rr := httptest.NewRecorder()
 
@@ -41,4 +42,55 @@ func TestPing(t *testing.T) {
 	body = bytes.TrimSpace(body)
 
 	assert.Equal(t, string(body), "OK")
+}
+
+func TestPingE2E(t *testing.T) {
+	// Create a new instance of our application struct. For now, this just
+	// contains a structured logger (which discards anything written to it).
+	app := &application{
+		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
+	}
+
+	// We then use the httptest.NewTLSServer() function to create a new test
+	// server, passing in the value returned by our app.routes() method as the
+	// handler for the server. This starts up a HTTPS server which listens on a
+	// randomly-chosen port of your local machine for the duration of the test.
+	// Notice that we defer a call to ts.Close() so that the server is shutdown
+	// when the test finishes.
+	ts := httptest.NewTLSServer(app.routes())
+	defer ts.Close()
+
+	// The network address that the test server is listening on is contained in
+	// the ts.URL field. We can  use this along with the ts.Client().Get() method
+	// to make a GET /ping request against the test server. This returns a
+	// http.Response struct containing the response.
+	rs, err := ts.Client().Get(ts.URL + "/ping")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We can then check the value of the response status code and body using
+	// the same pattern as before.
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
+}
+
+func TestPing(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, body := ts.get(t, "/ping")
+
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
